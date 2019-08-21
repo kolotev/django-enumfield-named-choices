002034cc7afd83e8d770715aaf697e885664ce54
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2019-08-20 20:28:31 -0400

    test for NamedBeerStyle were added

--- django_enumfield_named_choices/tests/test_validators.py
+++ django_enumfield_named_choices/tests/test_validators.py
@@ -1,5 +1,5 @@
 import unittest
-from django_enumfield_named_choices.tests.models import BeerStyle
+from django_enumfield_named_choices.tests.models import BeerStyle, NamedBeerStyle
 from django_enumfield_named_choices.validators import validate_available_choice
 from django_enumfield_named_choices.exceptions import InvalidStatusOperationError
 
@@ -30,4 +30,25 @@ class ValidatorTest(unittest.TestCase):
         """
         self.assertIsNone(
             validate_available_choice(BeerStyle, BeerStyle.name(BeerStyle.LAGER))
+        )
+
+    def test_validate_available_choice_5(self):
+        """Test passing a Name validation
+        """
+        self.assertIsNone(
+            validate_available_choice(BeerStyle, BeerStyle.value(BeerStyle.LAGER))
+        )
+
+    def test_validate_available_choice_6(self):
+        """Test passing a Name validation
+        """
+        self.assertIsNone(
+            validate_available_choice(NamedBeerStyle, NamedBeerStyle.name(NamedBeerStyle.LAGER))
+        )
+
+    def test_validate_available_choice_7(self):
+        """Test passing a Name validation
+        """
+        self.assertIsNone(
+            validate_available_choice(NamedBeerStyle, NamedBeerStyle.value(NamedBeerStyle.LAGER))
         )
